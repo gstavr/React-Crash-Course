947fa1039759d190489ebd37c97c382d5a01d572
Giorgos Stavrou <g.stavrou@epsilonet.gr>
2021-12-17 20:38:39 +0200

    All

--- react-course/src/App.test.js
+++ react-course/src/App.test.js
@@ -6,3 +6,12 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Sums numbers', () => {
+  expect(sum(1, 2)).toEqual(3);
+  expect(sum(2, 2)).toEqual(4);
+});
+
+function sum(a, b) {
+  return a + b;
+}
---
+++ react-course/src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,29 @@
+import Card from '../ui/Card';
+import classes from './NewMeetupForm.module.css';
+
+function NewMeetupForm(props) {
+  return (
+    <Card>
+      <form className={classes.form}>
+        <div className={classes.control}>
+          <label htmlFor='title'>Meetup Title</label>
+          <input type='text' required id='title' />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='image'>Meetup Image</label>
+          <input type='url' required id='image' />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='address'>Address</label>
+          <input type='text' required id='address' />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='image'>Meetup Image</label>
+          <input type='url' required id='image' />
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;

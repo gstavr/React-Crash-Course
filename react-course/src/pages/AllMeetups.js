172f6842ded75c20c378d37b27dc4d407a01352b
Giorgos Stavrou <g.stavrou@epsilonet.gr>
2021-12-17 22:05:49 +0200

    Ending today

--- react-course/src/pages/AllMeetups.js
+++ react-course/src/pages/AllMeetups.js
@@ -1,5 +1,5 @@
 import MeetupList from '../components/meetups/MeetupList';
-
+import { useState, useEffect } from 'react';
 const DUMMY_DATA = [
   {
     id: 'm1',
@@ -17,10 +17,41 @@ const DUMMY_DATA = [
   },
 ];
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://react-getting-started-86567-default-rtdb.firebaseio.com/meetups.json')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   return (
     <section>
       <h1>All Meetups Page</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 }
